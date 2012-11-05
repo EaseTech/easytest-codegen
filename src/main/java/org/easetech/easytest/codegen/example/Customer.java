af5486f3c70f97a29fb27b73985eedad4993997f
ravi-polampelli <ravi.polampelli@gmail.com>
2012-11-05 17:36:43 +0000

    initial version

--- src/main/java/org/easetech/easytest/codegen/example/Customer.java
+++ src/main/java/org/easetech/easytest/codegen/example/Customer.java
@@ -1,5 +1,38 @@
 package org.easetech.easytest.codegen.example;
 
-public class Customer {
+public class Customer
+{
+    private String _name;
+    private int    _zip;
 
+    public Customer (String name, int zip) {
+        _name = name;
+        _zip = zip;
+    }
+    
+    public Customer () {
+      super();
+    	_name = "defaultName";
+    	_zip = 12345;
+    }
+
+    public String getName () {
+        return _name;
+    }
+
+    public int getZip () {
+        return _zip;
+    }
+    
+    public void  setName (String name) {
+        this._name = name;
+    }
+
+    public void setZip(int zip) {
+        this._zip = zip;
+    }
+    
+    public String fetchNameZipCombined(String combined){
+    	return _name.concat(combined).concat(String.valueOf(_zip));
+    }
 }
