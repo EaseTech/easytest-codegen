ca3b960d64c666c4788f7daf6bf49b260091fad8
ravi-polampelli <ravi.polampelli@gmail.com>
2012-11-05 17:39:08 +0000

    initial version

--- src/main/java/org/easetech/easytest/codegen/example/dto/ItemCategory.java
+++ src/main/java/org/easetech/easytest/codegen/example/dto/ItemCategory.java
@@ -1,5 +1,26 @@
 package org.easetech.easytest.codegen.example.dto;
 
 public class ItemCategory {
+  
+	private int categoryId;
+	@Override
+	public String toString() {
+		return "ItemCategory [categoryId=" + categoryId + ", categoryName="
+				+ categoryName + "]";
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	private String categoryName;
 
 }
+
