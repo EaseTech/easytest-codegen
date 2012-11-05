7a81bc7a1d2ebe83191fc488e85912e0bb2fa929
ravi-polampelli <ravi.polampelli@gmail.com>
2012-11-05 17:37:11 +0000

    initial version

--- src/main/java/org/easetech/easytest/codegen/example/ItemService.java
+++ src/main/java/org/easetech/easytest/codegen/example/ItemService.java
@@ -1,5 +1,11 @@
 package org.easetech.easytest.codegen.example;
 
-public class ItemService {
+import java.util.List;
+
+public interface ItemService {
+
+    public List<Item> getItems(LibraryId libraryId, String searchText, String itemType);
+
+    public Item findItem(LibraryId libraryId, ItemId itemId);
 
 }
