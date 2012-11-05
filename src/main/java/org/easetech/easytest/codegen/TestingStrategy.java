9de2917a8fa9c4d1a4b3e931139ed7b64e207739
ravi-polampelli <ravi.polampelli@gmail.com>
2012-11-05 13:14:03 +0000

    initial version

--- src/main/java/org/easetech/easytest/codegen/TestingStrategy.java
+++ src/main/java/org/easetech/easytest/codegen/TestingStrategy.java
@@ -1,4 +1,11 @@
+/*
     This file is part of  EasyTest CodeGen, a project to generate 
+    JUnit test cases  from source code in EasyTest Template format and  helping to keep them in sync
+    during refactoring.
+ 	EasyTest CodeGen, a tool provided by
+	EaseTech Organization Under Apache License 2.0 
+	http://www.apache.org/licenses/LICENSE-2.0.txt
+*/
 
 package org.easetech.easytest.codegen;
 
